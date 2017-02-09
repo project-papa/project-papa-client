13434d84244713a4cd02d90bbb10165a00008027
Chris Jones <clj40@cam.ac.uk>
2017-02-09 11:52:24 +0000

    Added shapes to be used by world

---
+++ src/shape.ts
@@ -0,0 +1,49 @@
+import THREE from 'three';
+
+export interface Shape {
+  // Properties related directly to rendering of shape in three.js
+  readonly geometry : THREE.Geometry;
+  readonly material : THREE.Material;
+  readonly mesh : THREE.Mesh;
+
+  // Method exposes the mesh to be used by the World
+  getMesh() : THREE.Mesh;
+}
+
+export class Sphere implements Shape {
+  // Properties related directly to rendering of shape in three.js
+  /* Note: Properties are immutable for now, as unsure how the renderer
+     handles change to a Mesh once rendering has begun */
+  readonly geometry : THREE.SphereGeometry;
+  readonly material : THREE.Material;
+  readonly mesh : THREE.Mesh;
+
+  constructor(pos : THREE.Vector3, geo : THREE.SphereGeometry, mat : THREE.Material) {
+    this.geometry = geo;
+    this.material = mat;
+
+    // Instantiate the mesh from the geometry and the material
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+  }
+
+  getMesh() { return this.mesh; }
+}
+
+export class Box implements Shape {
+  // Properties related directly to rendering of shape in three.js
+  /* Note: Properties are immutable for now, as unsure how the renderer
+     handles change to a Mesh once rendering has begun */
+  readonly geometry : THREE.BoxGeometry;
+  readonly material : THREE.Material;
+  readonly mesh : THREE.Mesh;
+
+  constructor(pos : THREE.Vector3, geo : THREE.BoxGeometry, mat : THREE.Material) {
+    this.geometry = geo;
+    this.material = mat;
+
+    // Instantiate the mesh from the geometry and the material
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+  }
+
+  getMesh() { return this.mesh; }
+}
