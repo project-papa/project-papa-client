a8e35ae33391ebe05b5ee06a37ecbc82fa309e5b
Christian Silver <christian@decode.org.nz>
2017-03-01 12:07:07 +0000

    Add Entities as an abstraction of objects in the world

---
+++ src/entities/entity.ts
@@ -0,0 +1,16 @@
+import { World } from 'src/world';
+
+/**
+ * Encapsulates a logical entity in a world. Can be removed and added
+ */
+export interface Entity {
+  /**
+   * Actions to perform when an entity is added to a particular world
+   */
+  onAdd(world: World): void;
+
+  /**
+   * Actions to perform when an entity is removed from a particular world
+   */
+  onRemove(world: World): void;
+}
